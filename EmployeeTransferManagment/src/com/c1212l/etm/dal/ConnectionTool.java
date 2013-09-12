dbe78e4ad9dfd4f3e7f4f8b02653c79d35a86eed
duonghm56 <duonghm_56@vnu.edu.vn>
2013-09-13 00:08:50 +0700

    connection tool. Các class trong dal extends ConnectionTool

--- EmployeeTransferManagment/src/com/c1212l/etm/dal/ConnectionTool.java
+++ EmployeeTransferManagment/src/com/c1212l/etm/dal/ConnectionTool.java
@@ -4,10 +4,28 @@
  */
 package com.c1212l.etm.dal;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author OLDPC
  */
 public class ConnectionTool {
+    
+    Connection conn;
+    
+    public void initConnection() throws ClassNotFoundException, SQLException{
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        conn = DriverManager.getConnection("jdbc:sqlserver://localhost;databaseName=eproject","sa","1234$");        
+    }
+    
+    public void closeConnection() throws SQLException{
+        if(conn != null){
+            conn.close();
+            conn = null;
+        }
+    }   
     
 }
