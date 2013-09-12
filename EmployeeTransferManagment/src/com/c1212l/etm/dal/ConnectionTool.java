91025119caea778a4ee1193b1b7a606ce4df04ec
duonghm56 <duonghm_56@vnu.edu.vn>
2013-09-13 00:04:36 +0700

    fix error

---
+++ EmployeeTransferManagment/src/com/c1212l/etm/dal/ConnectionTool.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.c1212l.etm.dal;
+
+/**
+ *
+ * @author OLDPC
+ */
+public class ConnectionTool {
+    
+}
