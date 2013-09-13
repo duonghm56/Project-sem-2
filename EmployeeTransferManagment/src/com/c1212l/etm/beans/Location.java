52b2223ef664c92afe061ae5d7d934ca0a349c1a
luunguyenvan93 <taquocluu93@gmail.com>
2013-09-13 22:19:10 +0700

    store procedure department and loaction
    Add,Edit,Delete

---
+++ EmployeeTransferManagment/src/com/c1212l/etm/beans/Location.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.c1212l.etm.beans;
+
+/**
+ *
+ * @author Luu Bi
+ */
+public class Location {
+    private int locationID;
+    private String locationName;
+
+    public Location() {
+    }
+
+    public Location(int locationID, String locationName) {
+        this.locationID = locationID;
+        this.locationName = locationName;
+    }
+
+    public int getLocationID() {
+        return locationID;
+    }
+
+    public void setLocationID(int locationID) {
+        this.locationID = locationID;
+    }
+
+    public String getLocationName() {
+        return locationName;
+    }
+
+    public void setLocationName(String locationName) {
+        this.locationName = locationName;
+    }
+    
+}
