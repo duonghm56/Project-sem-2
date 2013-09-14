36612e1b5ce391efac833bb7b62f5ee4ba6ec33b
duonghm56 <duonghm_56@vnu.edu.vn>
2013-09-14 18:02:31 +0700

    TransferType DAO and DTO

---
+++ EmployeeTransferManagment/src/com/c1212l/etm/beans/TransferType.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.c1212l.etm.beans;
+
+/**
+ *
+ * @author OLDPC
+ */
+public class TransferType {
+    private int transferTypeID;
+    private String transferTypeName;
+    /*
+     * There are only 3 transferType : 
+     *   + only transfer Department
+     *   + only transfer Project
+     *   + transfer both Department and Project
+     */
+
+    public int getTransferTypeID() {
+        return transferTypeID;
+    }
+
+    public void setTransferTypeID(int transferTypeID) {
+        this.transferTypeID = transferTypeID;
+    }
+
+    public String getTransferTypeName() {
+        return transferTypeName;
+    }
+
+    public void setTransferTypeName(String transferTypeName) {
+        this.transferTypeName = transferTypeName;
+    }
+
+    public TransferType() {
+    }
+
+    public TransferType(int transferTypeID, String transferTypeName) {
+        this.transferTypeID = transferTypeID;
+        this.transferTypeName = transferTypeName;
+    }
+    
+}
---
+++ EmployeeTransferManagment/src/com/c1212l/etm/dal/TransferTypeDAO.java
@@ -0,0 +1,75 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.c1212l.etm.dal;
+
+import com.c1212l.etm.beans.TransferType;
+import java.sql.CallableStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author OLDPC
+ */
+public class TransferTypeDAO extends ConnectionTool{
+    
+    public ArrayList<TransferType> getAllTransferType() throws ClassNotFoundException, SQLException{
+        initConnection();
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery("select * from project");        
+        ArrayList<TransferType> result = new ArrayList<TransferType>();
+        while(rs.next()){
+            TransferType p = new TransferType();
+            p.setTransferTypeID(rs.getInt("transferTypeID"));
+            p.setTransferTypeName(rs.getString("transferTypeName"));
+            result.add(p);
+        }
+        closeConnection();
+        return result;
+    }
+    
+    public ArrayList<TransferType> searchTransferType(String condition) throws ClassNotFoundException, SQLException{
+        initConnection();
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery("select * from project " + condition);        
+        ArrayList<TransferType> result = new ArrayList<TransferType>();
+        while(rs.next()){
+            TransferType p = new TransferType();
+            p.setTransferTypeID(rs.getInt("transferTypeID"));
+            p.setTransferTypeName(rs.getString("transferTypeName"));
+            result.add(p);
+        }
+        closeConnection();
+        return result;
+    }
+    
+    public void addTransferType(TransferType transferType) throws ClassNotFoundException, SQLException{
+        initConnection();
+        CallableStatement cs = conn.prepareCall("{call addTransferType(?)}");
+        cs.setString(1, transferType.getTransferTypeName());        
+        cs.executeUpdate();
+        closeConnection();
+    }
+    
+    public void updateTransferType(TransferType transferType) throws ClassNotFoundException, SQLException{
+        initConnection();
+        CallableStatement cs = conn.prepareCall("{call updateTransferType(?, ?)}");
+        cs.setInt(1, transferType.getTransferTypeID());
+        cs.setString(2, transferType.getTransferTypeName());        
+        cs.executeUpdate();
+        closeConnection();
+    }
+    
+    public void deleteTransferType(TransferType transferType) throws ClassNotFoundException, SQLException{
+        initConnection();
+        CallableStatement cs = conn.prepareCall("{call deleteTransferType(?)}");
+        cs.setInt(1, transferType.getTransferTypeID());        
+        cs.executeUpdate();
+        closeConnection();
+    }
+    
+}
