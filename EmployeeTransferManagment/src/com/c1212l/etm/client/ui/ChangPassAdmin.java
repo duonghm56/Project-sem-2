4224126579de6f27520f0dfb7116324cf92bca7e
VuHoangBKAP <lulzsec1992@gmail.com>
2013-09-17 04:16:43 -0600

    packge emp
    package emp

--- EmployeeTransferManagment/src/com/c1212l/etm/client/ui/ChangPassAdmin.java
+++ EmployeeTransferManagment/src/com/c1212l/etm/client/ui/ChangPassAdmin.java
@@ -2,8 +2,9 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.C1212L.etm.UI;
+package com.c1212l.etm.client.ui;
 
+import com.c1212l.etm.ui.*;
 import javax.swing.JOptionPane;
 
 /**
